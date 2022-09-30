7dfadcda7a18f13735fc8529dd5a776a90521664
hezhongfeng <287645587@qq.com>
2022-09-30 13:50:19 +0800

    ✨ feat: add memory user

--- src/main/java/com/example/securityexample/security/config/DefaultSecurityConfig.java
+++ src/main/java/com/example/securityexample/security/config/DefaultSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 
@@ -46,6 +50,13 @@ public class DefaultSecurityConfig {
 		// @formatter:on
 
 		return http.build();
+	}
+
+	@Bean
+	UserDetailsService users() {
+		UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password")
+				.roles("USER").build();
+		return new InMemoryUserDetailsManager(user);
 	}
 
 }
